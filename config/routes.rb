3dd974dee92161e061b41a236cd388aa794eae78
youmari <youmari300@gmail.com>
2022-04-27 09:09:47 +0000

    Add seed file

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
       get 'message', to: 'messages#index'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "api/v1/messages#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greeting_atrributes = [
+  { text: "hello" },
+  { text: "haha" },
+  { text: "whats up!" },
+  { text: "hey dude!" },
+  { text: "nice smile!" }
+]
+puts "seeding data..."
+greeting_atrributes.each do |attributes|
+  Message.create(attributes) unless Message.where(attributes).first
+end
+
+puts "done!"
